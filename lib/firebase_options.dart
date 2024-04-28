36ac401304a06d973a6aa830700b0dba977a7040
hussen12bag <154493540+hussen12bag@users.noreply.github.com>
2024-04-28 15:31:51 +0300

    Block cahin

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -14,6 +14,8 @@ import 'package:flutter/foundation.dart'
 ///   options: DefaultFirebaseOptions.currentPlatform,
 /// );
 /// ```
+/// Bloack chain{
+/// }
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
